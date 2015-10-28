634f813c9699be72626b1b8e244af3f0efab6f32
RPing <g1222888@gmail.com>
2015-10-28 21:32:48 +0800

    [Commission problem] fix test code bug

--- src/commission.cpp
+++ src/commission.cpp
@@ -7,11 +7,11 @@ float commission(int lock, int stock, int barrel)
     if (lock == -1)
         return 0.0f;
     else if (lock < 1 || lock > 70)
-        return -1;
+        return -1.0f;
     if (stock < 1 || stock > 80)
-        return -1;
+        return -1.0f;
     if (barrel < 1 || barrel > 90)
-        return -1;
+        return -1.0f;
     sales = lock * 45.0f + stock * 30.0f + barrel * 25.0f;
     if (sales < 1000)
         commission = sales * 0.1f;
